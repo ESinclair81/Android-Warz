2f1d6370bb45577f2e0b9e7e51348d875040942c
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2021-01-03 17:31:09 -0500

    Start and end game funnctions added

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,20 +16,68 @@ console.log(playerName, playerAttack, playerHealth);
 // Alert players that they are starting the round
     window.alert("LET THE ANDROID WARZ BEGIN!!!");
 
-/*___________________________ For Loop to Cycle Through Enemies_______________*/
+    // function to start a new game
+var startGame = function() {
 
-for(var i = 0; i < enemyNames.length; i++) {
+    // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 
     for (var i = 0; i < enemyNames.length; i++) {
-        debugger;
-        // call fight function with enemy-robot
-        fight(enemyNames[i]);
+      if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  
+        var pickedEnemyName = enemyNames[i];
+  
+        enemyHealth = 50;
+  
+        fight(pickedEnemyName);
       }
+      else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+            // use debugger to pause script from running and check what's going on at that moment in the code
+            // debugger;
+        
+            // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+            fight(pickedEnemyName);
+          }
+
+           // play again
+//   startGame();
       
     console.log(enemyNames[i]);
     console.log(i);
     console.log(enemyNames[i] + " is at " + i + " index");
-  }
+
+    // function to end the entire game
+    var endGame = function() {
+        // if player is still alive, player wins!
+        if (playerHealth > 0) {
+          window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+        } 
+        else {
+          window.alert("You've lost your robot in battle.");
+        }
+
+        // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+      }
+      
+// after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+endGame();
+
+  };
   
 
   var fight = function(enemyName) {
@@ -80,11 +128,13 @@ for(var i = 0; i < enemyNames.length; i++) {
       // check player's health
       if (playerHealth <= 0) {
         window.alert(playerName + ' has died!');
+
         // leave while() loop if player is dead
         break;
       } else {
         window.alert(playerName + ' still has ' + playerHealth + ' health left.');
       }
+      
     }
   };    
 
@@ -92,3 +142,5 @@ for(var i = 0; i < enemyNames.length; i++) {
     fight(enemyNames[i]);
   }
 
+// start the game when the page loads
+startGame();
