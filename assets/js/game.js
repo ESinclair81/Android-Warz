43b12043b4a581ef1d9ed0a7faafe824f0766f84
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2021-01-03 21:53:56 -0500

    Fixing playerName prompt bugs

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,8 +5,15 @@ var randomNumber = function(min, max) {
   return value;
 };
 
-var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  var playerInfo = {
+    name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -72,6 +79,7 @@ var startGame = function() {
     for (var i = 0; i < enemyInfo.length; i++) {
       if (playerInfo.health > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        debugger;
   
         var pickedEnemyObj = enemyInfo[i];
   
